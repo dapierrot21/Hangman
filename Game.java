197792ed6558b10f9f0fc19f44ebcaf6edeeb68f
Dean Pierrot <dapierrot21@gmail.com>
2017-06-15 13:11:22 -0400

    Added more game logic

--- Game.java
+++ Game.java
@@ -5,12 +5,31 @@ class Game {
   private String misses;
 
   public Game(String answer) {
-    this.answer = answer;
+    this.answer = answer.toLowerCase();
     hits = "";
     misses = "";
   }
 
+  private char normalizeGuess(char letter) {
+    if(! Character.isLetter(letter)) {
+      throw new IllegalArgumentException("A letter is required.");
+    }
+    letter = Character.toLowerCase(letter);
+    if(misses.indexOf(letter) != -1 || hits.indexOf(letter) != -1) {
+      throw new IllegalArgumentException(letter + " has already been guessed.");
+    }
+    return letter;
+  }
+
+  public boolean applyGuess(String letters) {
+    if(letters.length() == 0) {
+      throw new IllegalArgumentException("No letters where found");
+    }
+    return applyGuess(letters.charAt(0));
+  }
+
   public boolean applyGuess(char letter) {
+    letter = normalizeGuess(letter);
     boolean isHit = answer.indexOf(letter) != -1;
     if(isHit) {
       hits += letter;
